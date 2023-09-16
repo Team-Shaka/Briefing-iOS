1c7f7683205f4712238c43b9f02373556dd2401a
Jeonhui <dlwjsgml02@naver.com>
2023-09-17 05:05:14 +0900

    FEAT: Briefing Main UI

--- Briefing/MainViewController.swift
+++ Briefing/MainViewController.swift
@@ -8,19 +8,41 @@
 import UIKit
 import FSCalendar
 
-class MainViewController: UIViewController {
+final class MainViewController: UIViewController {
     var selectedDate = Date()
+    
+    private var navigationView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private var titleLabel: UIView = {
+        let label = UILabel()
+        label.text = "Briefing"
+        label.font = .productSans(size: 24)
+        label.textColor = .briefingWhite
+        return label
+    }()
+    
+    private var storageButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "storage"), for: .normal)
+        button.contentMode = .scaleAspectFit
+        return button
+    }()
+    
+    private var settingButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "setting"), for: .normal)
+        button.contentMode = .scaleAspectFit
+        return button
+    }()
     
     private var calendarGuideView: UIView = {
         let view = UIView()
         view.clipsToBounds = true
         return view
     }()
-    
-    private var pageViewController: UIPageViewController {
-        let pageViewController = UIPageViewController()
-        return pageViewController
-    }
     
     private lazy var calendarView: FSCalendar = {
         let calendarView = FSCalendar()
@@ -40,7 +62,25 @@ class MainViewController: UIViewController {
         return calendarView
     }()
     
-    var calendarHeight: NSLayoutConstraint!
+    private lazy var pageViewController: UIPageViewController = {
+        let pageViewController = UIPageViewController(transitionStyle: .scroll,
+                                                      navigationOrientation: .horizontal)
+        
+        return pageViewController
+    }()
+    
+    private var briefingViewControllers: [UIViewController] = {
+        let today = Date()
+        return [
+            MainBriefingViewController(briefingDate: today.date(byAdding: .day, value: -6) ?? today),
+            MainBriefingViewController(briefingDate: today.date(byAdding: .day, value: -5) ?? today),
+            MainBriefingViewController(briefingDate: today.date(byAdding: .day, value: -4) ?? today),
+            MainBriefingViewController(briefingDate: today.date(byAdding: .day, value: -3) ?? today),
+            MainBriefingViewController(briefingDate: today.date(byAdding: .day, value: -2) ?? today),
+            MainBriefingViewController(briefingDate: today.date(byAdding: .day, value: -1) ?? today),
+            MainBriefingViewController(briefingDate: today)
+        ]
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,25 +91,74 @@ class MainViewController: UIViewController {
     
     private func configure() {
         view.backgroundColor = .briefingBlue
+        navigationItem.title = "Briefing"
+        navigationController?.isNavigationBarHidden = true
+        
+        
         calendarView.delegate = self
         calendarView.dataSource = self
         calendarView.register(MainCalendarCell.self,
                               forCellReuseIdentifier: MainCalendarCell.identifier)
         
+        pageViewController.delegate = self
+        pageViewController.dataSource = self
+        addChild(pageViewController)
+        if let currentBriefingViewController = briefingViewControllers.last {
+            pageViewController.setViewControllers([currentBriefingViewController],
+                                                  direction: .forward,
+                                                  animated: true)
+        }
     }
     
     private func addSubviews() {
         calendarGuideView.addSubview(calendarView)
         
-        let subViews: [UIView] = [calendarGuideView]
+        let navigationSubviews: [UIView] = [titleLabel,
+                                            storageButton,
+                                            settingButton]
+        navigationSubviews.forEach { subView in
+            navigationView.addSubview(subView)
+        }
+        
+        let subViews: [UIView] = [navigationView,
+                                  calendarGuideView,
+                                  pageViewController.view]
         subViews.forEach { subView in
             view.addSubview(subView)
         }
     }
     
     private func makeConstraints() {
+        navigationView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalTo(titleLabel).offset(6)
+            make.leading.trailing.equalTo(view)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(navigationView).offset(6)
+            make.centerY.equalTo(navigationView)
+            make.leading.equalTo(navigationView).inset(28)
+            make.trailing.lessThanOrEqualTo(storageButton)
+        }
+        
+        storageButton.snp.makeConstraints { make in
+            make.height.equalTo(navigationView)
+            make.width.equalTo(storageButton.snp.height)
+            make.trailing.equalTo(settingButton.snp.leading).offset(-2)
+            make.centerY.equalTo(navigationView)
+        }
+        
+        settingButton.snp.makeConstraints { make in
+            make.height.equalTo(navigationView)
+            make.width.equalTo(settingButton.snp.height)
+            make.trailing.equalTo(navigationView).inset(18)
+            make.centerY.equalTo(navigationView)
+        }
+        
         calendarGuideView.snp.makeConstraints { make in
-            make.top.centerX.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(navigationView.snp.bottom)
+            make.centerX.equalTo(view.safeAreaLayoutGuide)
             make.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(6)
             make.height.equalTo(80)
         }
@@ -79,23 +168,50 @@ class MainViewController: UIViewController {
             make.bottom.equalTo(view)
         }
         
+        pageViewController.view.snp.makeConstraints { make in
+            make.top.equalTo(calendarGuideView.snp.bottom).offset(10)
+            make.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            make.bottom.equalTo(view)
+        }
     }
     
+    // FIXME: - PageViewController & Calendar Sync
     func changeSelectedDateAction(_ date: Date) {
         selectedDate = date
+        if calendarView.selectedDate != selectedDate {
+            calendarView.select(selectedDate)
+        }
+        if let viewController = pageViewController.viewControllers?.first,
+           let breifingViewController = viewController as? MainBriefingViewController {
+            
+            if breifingViewController.briefingDate != selectedDate,
+               let currentBreifingViewController = briefingViewControllers.filter({ vc in
+                   print(vc, selectedDate)
+                   return (vc as? MainBriefingViewController)?.briefingDate == selectedDate
+               }).first {
+                let direction: UIPageViewController.NavigationDirection = breifingViewController.briefingDate > selectedDate ? .reverse : .forward
+                pageViewController.setViewControllers([currentBreifingViewController],
+                                                      direction: direction,
+                                                      animated: true)
+            }
+        }
     }
 }
 
 extension MainViewController: FSCalendarDelegate,
                               FSCalendarDataSource,
                               FSCalendarDelegateAppearance {
-    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+    
+    func calendar(_ calendar: FSCalendar,
+                  didSelect date: Date,
+                  at monthPosition: FSCalendarMonthPosition) {
         // MARK: - 전체 날짜 선택 가능시 calendarScrollDateToCenter
         // calendarScrollDateToCenter(calendar, selectedDate: date)
         changeSelectedDateAction(date)
     }
     
-    func calendarScrollDateToCenter(_ calendar: FSCalendar, selectedDate: Date) {
+    func calendarScrollDateToCenter(_ calendar: FSCalendar,
+                                    selectedDate: Date) {
         guard let indexPath = calendar.collectionView.indexPathsForSelectedItems?.first else { return }
         let numOfDays = (indexPath.section * 7 + indexPath.row - 3)
         let section = Int(numOfDays / 7)
@@ -112,11 +228,15 @@ extension MainViewController: FSCalendarDelegate,
         return Date()
     }
     
-    func calendar(_ calendar: FSCalendar, shouldDeselect date: Date, at monthPosition: FSCalendarMonthPosition) -> Bool {
+    func calendar(_ calendar: FSCalendar,
+                  shouldDeselect date: Date,
+                  at monthPosition: FSCalendarMonthPosition) -> Bool {
         return false
     }
     
-    func calendar(_ calendar: FSCalendar, cellFor date: Date, at position: FSCalendarMonthPosition) -> FSCalendarCell {
+    func calendar(_ calendar: FSCalendar,
+                  cellFor date: Date,
+                  at position: FSCalendarMonthPosition) -> FSCalendarCell {
         guard let cell: MainCalendarCell = calendar.dequeueReusableCell(withIdentifier: MainCalendarCell.identifier,
                                                                         for: date,
                                                                         at: position) as? MainCalendarCell else {
@@ -125,5 +245,33 @@ extension MainViewController: FSCalendarDelegate,
         cell.setDate(date)
         cell.isUserInteractionEnabled = true
         return cell
+    }
+}
+
+extension MainViewController: UIPageViewControllerDelegate,
+                              UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let index = briefingViewControllers.firstIndex(of: viewController),
+              let viewController = briefingViewControllers[safe: index - 1] else { return nil }
+        return viewController
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = briefingViewControllers.firstIndex(of: viewController),
+              let viewController = briefingViewControllers[safe: index - 1] else { return nil }
+        return viewController
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            didFinishAnimating finished: Bool,
+                            previousViewControllers: [UIViewController],
+                            transitionCompleted completed: Bool) {
+        guard let selectdBriefingViewController = pageViewController.viewControllers?.first
+                as? MainBriefingViewController else {
+            return
+        }
+        self.changeSelectedDateAction(selectdBriefingViewController.briefingDate)
     }
 }
--- Briefing/Utility/Date+.swift
+++ Briefing/Utility/Date+.swift
@@ -14,10 +14,29 @@ extension Date {
         Calendar.current.component(.weekday, from: self) - 1
     }
     
+    var dateToIntSet: (year: Int, month: Int, day: Int) {
+        let calendar = Calendar.current
+        let year = calendar.component(.year, from: self)
+        let month = calendar.component(.month, from: self)
+        let day = calendar.component(.day, from: self)
+        return (year: year, month: month, day: day)
+    }
+    
     static func date(year: Int, month: Int, day: Int) -> Date? {
         let dateStr = "\(year)-\(month)-\(day)"
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
         return dateFormatter.date(from: dateStr)
+    }
+    
+    func date(byAdding: Calendar.Component, value: Int) -> Date? {
+        let calendar = Calendar.current
+        return calendar.date(byAdding: byAdding, value: value, to: self)
+    }
+    
+    func dateToString(_ dateFormat: String = "yyyy.MM.dd") -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = dateFormat
+        return dateFormatter.string(from: self)
     }
 }
