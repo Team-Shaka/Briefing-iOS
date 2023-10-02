200209094e8226f2168804ac5fc0b45866a01ef6
Jeonhui <dlwjsgml02@naver.com>
2023-10-02 20:10:56 +0900

    FEAT: AuthManager

---
+++ Briefing/Models/Auth/BriefingAuthManager.swift
@@ -0,0 +1,24 @@
+//
+//  BriefingAuthManager.swift
+//  Briefing
+//
+//  Created by 이전희 on 2023/10/02.
+//
+
+import Foundation
+
+final class BriefingAuthManager {
+    static let shared: BriefingAuthManager = BriefingAuthManager()
+    private init() { }
+    
+}
+
+extension BriefingAuthManager {
+    func googleSignIn() {
+        
+    }
+    
+    func appleSignIn() {
+        
+    }
+}
