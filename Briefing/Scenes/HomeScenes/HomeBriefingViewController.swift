5c3e059012dba69c0106b95f4c5f2e230921fe4e
Jeonhui <dlwjsgml02@naver.com>
2023-10-04 00:24:42 +0900

    FEAT: BriefingTableView

--- Briefing/Scenes/HomeScenes/HomeBriefingViewController.swift
+++ Briefing/Scenes/HomeScenes/HomeBriefingViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 final class HomeBriefingViewController: UIViewController {
     private let networkManager = BriefingNetworkManager.shared
     var briefingDate: Date
+    var keywords: Keywords? = nil
     
     var briefingTitleLabel: UILabel = {
         let label = UILabel()
@@ -23,6 +24,23 @@ final class HomeBriefingViewController: UIViewController {
         label.font = .productSans(size: 12, weight: .regular)
         label.textColor = .briefingLightBlue
         return label
+    }()
+    
+    var keywordBriefingTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.rowHeight = 86
+        tableView.separatorStyle = .none
+        tableView.allowsSelection = false
+        tableView.backgroundColor = .clear
+        tableView.showsVerticalScrollIndicator = false
+        tableView.sectionHeaderHeight = 14
+        tableView.sectionHeaderTopPadding = 0
+        return tableView
+    }()
+    
+    private var tableViewHeaderView: UIView = {
+        let view = UIView()
+        return view
     }()
     
     init(briefingDate: Date) {
@@ -58,11 +76,18 @@ final class HomeBriefingViewController: UIViewController {
                                                    localeIdentifier: BriefingStringCollection.Locale.en)
         let updateTimeLabelText = "\(BriefingStringCollection.updated): \(updateTimeString)"
         briefingUpdateTimeLabel.text = updateTimeLabelText
+        
+        keywordBriefingTableView.delegate = self
+        keywordBriefingTableView.dataSource = self
+        keywordBriefingTableView.register(HomeBriefingTableViewCell.self,
+                                          forCellReuseIdentifier: HomeBriefingTableViewCell.identifier)
+        
     }
     
     private func addSubviews() {
         let subViews: [UIView] = [briefingTitleLabel,
-                                  briefingUpdateTimeLabel]
+                                  briefingUpdateTimeLabel,
+                                  keywordBriefingTableView]
         subViews.forEach { subView in
             view.addSubview(subView)
         }
@@ -79,15 +104,53 @@ final class HomeBriefingViewController: UIViewController {
             make.leading.equalToSuperview().offset(22)
             make.trailing.lessThanOrEqualToSuperview().offset(22)
         }
-    }
-    
-    private func fetchKeywords(){
-        print(briefingDate.dateToString())
-        networkManager.fetchKeywords(date: briefingDate,
-                                     type: .korea) { value, error in
-           
-            print(value, error)
+        keywordBriefingTableView.snp.makeConstraints { make in
+            make.top.equalTo(briefingUpdateTimeLabel.snp.bottom).offset(4)
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
         }
     }
     
+    private func fetchKeywords() {
+        networkManager.fetchKeywords(date: briefingDate,
+                                     type: .korea) { [weak self] value, error in
+            self?.keywords = value
+            self?.tableViewHeaderView.layer.sublayers?.first?.frame = self?.tableViewHeaderView.bounds ?? .zero
+            self?.keywordBriefingTableView.reloadData()
+        }
+    }
+}
+
+extension HomeBriefingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return self.keywords?.briefings.count ?? 0
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: HomeBriefingTableViewCell.identifier) as? HomeBriefingTableViewCell else {
+            return UITableViewCell()
+        }
+        cell.updateKeywordBriefing(KeywordBriefing(id: indexPath.row,
+                                                   ranks: indexPath.row + 1,
+                                                   title: "Title \(indexPath.row)",
+                                                   subTitle: "SubTitle \(indexPath.row)"))
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let gradient = CAGradientLayer()
+        gradient.frame = tableViewHeaderView.bounds
+        gradient.locations = [0.0, 0.8]
+        gradient.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradient.endPoint = CGPoint(x: 0.5, y: 1.0)
+        gradient.colors = [UIColor.briefingWhite.cgColor,
+                           UIColor.briefingWhite.withAlphaComponent(0.0).cgColor]
+        tableViewHeaderView.layer.addSublayer(gradient)
+        return tableViewHeaderView
+    }
+    
+    // func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    //     return 8
+    // }
 }
