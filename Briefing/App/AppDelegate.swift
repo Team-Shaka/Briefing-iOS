bf0cb281d9a8a9b4181cd2530c8cfdf1a10b92c8
Jeonhui <dlwjsgml02@naver.com>
2023-10-07 19:44:59 +0900

    FIX: Change Variable Access

--- Briefing/App/AppDelegate.swift
+++ Briefing/App/AppDelegate.swift
@@ -14,8 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        print(BriefingAuthManager.shared.member)
-        
         let center = UNUserNotificationCenter.current()
             center.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
         }
