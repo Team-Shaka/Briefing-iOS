3e11385c5137305b1d1e282e5cc0f96ba29cd13d
Jeonhui <dlwjsgml02@naver.com>
2023-10-31 18:15:38 +0900

    FEAT: Add Widget

---
+++ Briefing-Widget/Briefing_WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  Briefing_WidgetBundle.swift
+//  Briefing-Widget
+//
+//  Created by 이전희 on 10/31/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Briefing_WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Briefing_Widget()
+    }
+}
