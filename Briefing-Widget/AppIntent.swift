cab18d9fb192884fdfcb322fe063d6d56762b80d
Jeonhui <dlwjsgml02@naver.com>
2023-10-31 18:15:38 +0900

    FEAT: Add Widget

---
+++ Briefing-Widget/AppIntent.swift
@@ -0,0 +1,18 @@
+//
+//  AppIntent.swift
+//  Briefing-Widget
+//
+//  Created by 이전희 on 10/31/23.
+//
+
+import WidgetKit
+import AppIntents
+
+struct ConfigurationAppIntent: WidgetConfigurationIntent {
+    static var title: LocalizedStringResource = "Configuration"
+    static var description = IntentDescription("This is an example widget.")
+
+    // An example configurable parameter.
+    @Parameter(title: "Favorite Emoji", default: "😃")
+    var favoriteEmoji: String
+}
